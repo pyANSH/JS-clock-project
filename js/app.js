ae349508dde9eef995ddb6cc632517d64dea2904
AYAN SHRIVASTAVA <ayanshrivastava2020@gmail.com>
2021-08-24 23:43:16 +0530

    final update

---
+++ js/app.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    d = new Date();
+    hh = d.getHours();
+    mm = d.getMinutes();
+    ss = d.getSeconds();
+    hhr = 30*hh + mm/2;
+    mmr = 6*mm;
+    ssr = 6*ss;
+
+    hour.style.transform = `rotate(${hhr}deg)`;
+    minute.style.transform = `rotate(${mmr}deg)`;
+    second5.style.transform = `rotate(${ssr}deg)`;
+}, 1000);
